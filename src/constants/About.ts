66b96298bbb6a031c30e49e7dc38d99912de3cc9
Eric <eriicsilva28@gmail.com>
2024-11-05 17:17:42 -0300

    feat: Implement about constants

---
+++ src/constants/About.ts
@@ -0,0 +1,13 @@
+const AboutSections = [
+  { title: 'historyTitle', description: 'historyDescription' },
+  { title: 'missionTitle', description: 'missionDescription' },
+  { title: 'visionTitle', description: 'visionDescription' },
+  { title: 'teamTitle', description: 'teamDescription' },
+  { title: 'activitiesTitle', description: 'activitiesDescription' },
+  { title: 'partnersTitle', description: 'partnersDescription' }
+]
+
+const AboutValues = ['value1', 'value2', 'value3', 'value4']
+const AboutTestimonials = ['testimonial1', 'testimonial2']
+
+export { AboutSections, AboutTestimonials, AboutValues }
