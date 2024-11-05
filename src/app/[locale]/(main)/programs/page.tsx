33f730e467a2dc78cbd116a6471034fdaa1dd8e7
Eric <eriicsilva28@gmail.com>
2024-11-05 12:24:31 -0300

    feat: organize folder structure with Group Routes for main, community

---
+++ src/app/[locale]/(main)/programs/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is the about page</p>
+      <Link href='/'>Home</Link>
+    </div>
+  )
+}
+
+export default AboutPage
