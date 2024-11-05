3c7ef930d40d8af9675866aed95228275d0d9bfa
Eric <eriicsilva28@gmail.com>
2024-11-05 11:47:57 -0300

    feat: add i18n configuration for internationalization support

---
+++ src/i18n.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { getRequestConfig } from 'next-intl/server'
+import { notFound } from 'next/navigation'
+
+const locales = ['en', 'pt']
+
+export default getRequestConfig(async ({ requestLocale }) => {
+  const locale = await requestLocale
+
+  if (!locales.includes(locale as any)) {
+    notFound()
+  }
+
+  return {
+    messages: (await import(`../messages/${locale}.json`)).default
+  }
+})
