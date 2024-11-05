2b8dd633ba2ce52f06d6169bfb6910d18d17e165
Eric <eriicsilva28@gmail.com>
2024-11-05 15:29:36 -0300

    chore: update navigation paths to include community and programs routes

---
+++ src/navigation.ts
@@ -0,0 +1,22 @@
+import { createNavigation } from 'next-intl/navigation'
+
+import { LocalePrefix, Pathnames } from 'next-intl/routing'
+export const locales = ['en', 'pt'] as const
+
+export type Locales = typeof locales
+
+export const pathnames: Pathnames<Locales> = {
+  home: '/',
+  create: '/home',
+  pet: '/about',
+  petEdit: '/contact'
+}
+
+export const localePrefix: LocalePrefix<Locales> = 'always'
+
+export const { Link, getPathname, redirect, usePathname, useRouter } =
+  createNavigation({
+    locales,
+    localePrefix,
+    pathnames
+  })
