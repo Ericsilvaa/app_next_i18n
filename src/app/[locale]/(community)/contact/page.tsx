1479ae51a427c4b78536bb16a265b43773bb621b
Eric <eriicsilva28@gmail.com>
2024-11-06 21:41:14 -0300

    refactor: update Contact component to redirect to Feedback page after submission

--- src/app/[locale]/(community)/contact/page.tsx
+++ src/app/[locale]/(community)/contact/page.tsx
@@ -1,16 +1,16 @@
 'use client'
-import FeedBack from '@/components/common/FeedBack'
 import ContactForm from '@/components/forms/ContactForm'
 
 import { toast } from '@/hooks/use-toast'
+import { useRouter } from '@/navigation'
 import { useTranslations } from 'next-intl'
 
 import { useState } from 'react'
 
 export default function Contact() {
+  const router = useRouter()
   const t = useTranslations('Contact')
   const [isSubmitting, setIsSubmitting] = useState(false)
-  const [submitted, setSubmitted] = useState(false)
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
@@ -23,18 +23,14 @@ export default function Contact() {
       description: t('contactInstructions')
     })
 
-    setSubmitted(true)
     setIsSubmitting(false)
     event.currentTarget.reset()
+    router.replace('/feedback')
   }
 
   return (
     <div className='max-w-3xl mx-auto p-6 space-y-12'>
-      {!submitted && (
-        <ContactForm handleSubmit={handleSubmit} isSubmitting={isSubmitting} />
-      )}
-
-      {submitted && <FeedBack />}
+      <ContactForm handleSubmit={handleSubmit} isSubmitting={isSubmitting} />
     </div>
   )
 }
