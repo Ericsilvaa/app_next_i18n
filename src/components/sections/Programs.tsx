adc7a170762af1a177248446197f4c1c4403688e
Eric <eriicsilva28@gmail.com>
2024-11-06 22:53:51 -0300

    feat: reate Programs section to display available programs with details.

--- src/components/sections/Programs.tsx
+++ src/components/sections/Programs.tsx
@@ -1,35 +1,14 @@
 'use client'
+import { ProgramsSectionType } from '@/constants/Programs'
 import { useRouter } from '@/navigation'
 import { useTranslations } from 'next-intl'
 
-const ProgramsSection = () => {
+const ProgramsSection = ({ programs }: { programs: ProgramsSectionType[] }) => {
   const router = useRouter()
   const t = useTranslations('Programs')
 
-  const programs = [
-    {
-      key: 'educationProgram',
-      title: t('educationProgram.title'),
-      description: t('educationProgram.description')
-    },
-    {
-      key: 'volunteerProgram',
-      title: t('volunteerProgram.title'),
-      description: t('volunteerProgram.description')
-    },
-    {
-      key: 'undergraduate',
-      title: t('undergraduate.title'),
-      description: t('undergraduate.description')
-    },
-    {
-      key: 'graduate',
-      title: t('graduate.title'),
-      description: t('graduate.description')
-    }
-  ]
-
   const handleLearnMoreClick = (programId: string) => {
+    console.log('🚀 ~ handleLearnMoreClick ~ programId:', programId)
     router.push(`/programs/${programId}`)
   }
 
@@ -47,9 +26,9 @@ const ProgramsSection = () => {
             className='bg-white border border-gray-200 p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-200'
           >
             <h3 className='text-xl font-semibold text-blue-700 mb-3'>
-              {program.title}
+              {t(program.title)}
             </h3>
-            <p className='text-gray-600 text-sm'>{program.description}</p>
+            <p className='text-gray-600 text-sm'>{t(program.description)}</p>
             <button
               onClick={() => handleLearnMoreClick(program.key)}
               className='mt-4 text-blue-600 font-semibold hover:underline'
